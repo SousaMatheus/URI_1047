ceeb171d815a6446e68211d7ecb8e43c89f1b320
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-09-13 22:08:33 -0300

    adicionado mais possibilidades de horas, porém com 5% de erro

--- Program.cs
+++ Program.cs
@@ -15,30 +15,80 @@ namespace URI_1047
             minutoFinal = int.Parse(vet[3]);
                 //duracao de 24h
             if (horaInicial == horaFinal && minutoInicial == minutoFinal){
-                Console.ReadLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+                Console.WriteLine("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
             }
             else if (horaFinal > horaInicial && minutoFinal > minutoInicial){
-                duracaoH = horaFinal - horaFinal;
-                duracaoM = minutoFinal - minutoInicial;
-                Console.ReadLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = minutoFinal - minutoInicial; 
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");              
             }
             else if (horaFinal > horaInicial && minutoFinal == minutoInicial){
-                duracaoH = horaFinal - horaFinal;
-                duracaoM = 0;
-                 Console.ReadLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = 0;                 
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
             }
             else if ( horaFinal == horaInicial && minutoFinal > minutoInicial){
                 duracaoH = 0;
-                duracaoM = minutoFinal - minutoFinal;
-                Console.ReadLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+                duracaoM = minutoFinal - minutoInicial;    
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");           
             }
-            
-            
-
-
-
-
-
+            else if ( horaFinal < horaInicial && minutoFinal > minutoInicial){
+                duracaoH = 24 - horaInicial + horaFinal;
+                duracaoM = minutoFinal - minutoInicial; 
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");               
+            }
+            else if (horaFinal < horaInicial && minutoFinal < minutoInicial){
+                duracaoH = 24 - horaInicial + horaFinal - 1;                
+                duracaoM = (minutoFinal + 60) - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");                 
+            }       
+            else if (horaFinal > horaInicial && minutoFinal < minutoInicial){
+                duracaoH = horaFinal - horaInicial -1;
+                duracaoM = (minutoFinal + 60) - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");  
+            }       
+            // seria horaFinal < horaInicial && minutoFinal == minutoInicial
+            else if (horaFinal < horaInicial && minutoFinal == minutoInicial){
+                duracaoH = 24 - horaInicial + horaFinal - 1;
+                duracaoM = 0;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");  
+            }  
+            else if (horaFinal == 0 && minutoFinal > minutoInicial){
+                horaFinal = 24;
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = minutoFinal - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            } 
+            else if (horaFinal == 0 && minutoFinal < minutoInicial){
+                horaFinal = 24;
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = (minutoFinal + 60) - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            }
+            else if (horaFinal == 0 && minutoFinal == minutoInicial){
+                horaFinal = 24;
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = 0;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            }
+            else if (horaInicial == 0 && minutoFinal > minutoInicial){
+                horaInicial = 24;
+                 duracaoH = horaFinal - horaInicial;
+                duracaoM = minutoFinal - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            }
+            else if (horaInicial == 0 && minutoFinal < minutoInicial){
+                horaInicial = 24;
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = (minutoFinal + 60) - minutoInicial;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            }      
+            else {
+                horaInicial = 24;
+                duracaoH = horaFinal - horaInicial;
+                duracaoM = 0;
+                Console.WriteLine("O JOGO DUROU " + duracaoH + " HORA(S) E " + duracaoM + " MINUTO(S)");
+            }     
         }
     }
 }
